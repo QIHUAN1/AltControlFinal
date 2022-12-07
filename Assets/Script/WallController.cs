1f9f899bb39bbb137f538f2f779d3f6ab8a2d3f9
QIHUAN1 <qjiang1@inside.artcenter.edu>
2022-12-06 21:48:24 -0800

    111

--- Assets/Script/WallController.cs
+++ Assets/Script/WallController.cs
@@ -6,11 +6,18 @@ public class WallController : MonoBehaviour
 {
     AudioSource audiosource;
     public AudioClip WallSound;
+    public AudioClip PlayerSound;
+
+    public GameObject audio;
+
 
     // Start is called before the first frame update
     void Start()
     {
         audiosource = GetComponent<AudioSource>();
+        audio = GameObject.FindGameObjectWithTag("Audio");
+
+        
     }
 
     // Update is called once per frame
@@ -26,6 +33,21 @@ public class WallController : MonoBehaviour
             GetComponent<AudioSource>().clip = WallSound;
             GetComponent<AudioSource>().Play();
             Destroy(other.gameObject);
+        }
+
+        if (other.CompareTag("Player"))
+        {
+            audio.GetComponent<AudioSource>().clip = PlayerSound;
+            audio.GetComponent<AudioSource>().Play();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+
+            audio.GetComponent<AudioSource>().Pause();
         }
     }
 }
