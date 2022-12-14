92f1ad467ddecca37220772e55fc53e0dc9ae88d
QIHUAN1 <qjiang1@inside.artcenter.edu>
2022-12-14 00:56:51 -0800

    1

--- Assets/Script/AvatarController.cs
+++ Assets/Script/AvatarController.cs
@@ -15,65 +15,166 @@ public class AvatarController : MonoBehaviour
     public AudioClip RotateSound;
     public AudioClip RotateSound2;
 
+    public bool a45;
+    public bool a90;
+    public bool a135;
+    public bool a180;
+    public bool a225;
+    public bool a270;
+    public bool a315;
+    public bool a360;
+
+
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         audiosource = GetComponent<AudioSource>();
+
+        a360 = true;
+
+        a45 = false;
+        a90 = false;
+        a135 = false;
+        a180= false;
+        a225= false;
+        a270= false;
+        a315= false;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
-
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W) && a45 == false)
         {
             animator.SetTrigger("45");
             GetComponent<AudioSource>().clip = RotateSound;
             GetComponent<AudioSource>().Play();
+
+            a45 = true;
+
+            a90 = false;
+            a135 = false;
+            a180 = false;
+            a225 = false;
+            a270 = false;
+            a315 = false;
+            a360 = false;
         }
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) && a90 == false)
         {
             animator.SetTrigger("90");
             GetComponent<AudioSource>().clip = RotateSound;
             GetComponent<AudioSource>().Play();
+
+            a90 = true;
+
+            a45 = false;
+            a135 = false;
+            a180 = false;
+            a225 = false;
+            a270 = false;
+            a315 = false;
+            a360 = false;
         }
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S)&& a135 == false)
         {
             animator.SetTrigger("135");
             GetComponent<AudioSource>().clip = RotateSound;
             GetComponent<AudioSource>().Play();
+
+            a135 = true;
+
+            a90 = false;
+            a45 = false;
+            a180 = false;
+            a225 = false;
+            a270 = false;
+            a315 = false;
+            a360 = false;
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) && a180 == false)
         {
             animator.SetTrigger("180");
             GetComponent<AudioSource>().clip = RotateSound;
             GetComponent<AudioSource>().Play();
+
+            a180 = true;
+
+            a135 = false;
+            a90 = false;
+            a45 = false;
+            a225 = false;
+            a270 = false;
+            a315 = false;
+            a360 = false;
         }
-        if (Input.GetKey(KeyCode.F))
+        if (Input.GetKey(KeyCode.F) && a225 == false)
         {
             animator.SetTrigger("225");
             GetComponent<AudioSource>().clip = RotateSound;
             GetComponent<AudioSource>().Play();
+
+            a225 = true;
+
+            a180 = false;
+            a135 = false;
+            a90 = false;
+            a45 = false;
+            a270 = false;
+            a315 = false;
+            a360 = false;
         }
-        if (Input.GetKey(KeyCode.G))
+        if (Input.GetKey(KeyCode.G) && a270 == false)
         {
             animator.SetTrigger("270");
             GetComponent<AudioSource>().clip = RotateSound;
             GetComponent<AudioSource>().Play();
+
+            a270 = true;
+
+            a225 = false;
+            a180 = false;
+            a135 = false;
+            a90 = false;
+            a45 = false;
+            a315 = false;
+            a360 = false;
         }
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow) && a315 == false)
         {
             animator.SetTrigger("315");
             GetComponent<AudioSource>().clip = RotateSound;
             GetComponent<AudioSource>().Play();
+
+            a315 = true;
+
+            a270 = false;
+            a225 = false;
+            a180 = false;
+            a135 = false;
+            a90 = false;
+            a45 = false;
+            a360 = false;
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.DownArrow)&& a360 == false)
         {
             animator.SetTrigger("360");
             GetComponent<AudioSource>().clip = RotateSound2;
             GetComponent<AudioSource>().Play();
+
+            a360 = true;
+            a315 = false;
+            a270 = false;
+            a225 = false;
+            a180 = false;
+            a135 = false;
+            a90 = false;
+            a45 = false;
+
         }
     }
 
